1447eec3583563b8c4951b1b73f756c72c090515
Magaly <magaly@example.com>
2025-05-12 19:34:05 +0300

    mag

--- src/main/java/com/example/oopproject/config/SecurityConfig.java
+++ src/main/java/com/example/oopproject/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs/**").permitAll()
                         .requestMatchers("/auth/**", "/api/auth/**").permitAll()
-                        .requestMatchers("/api/users/register").permitAll()
+                        .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
