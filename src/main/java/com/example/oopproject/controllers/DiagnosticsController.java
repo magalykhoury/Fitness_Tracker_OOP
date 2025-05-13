02956e5561825f92352d319c6de24aa1f077de7f
Magaly <magaly@example.com>
2025-05-13 21:14:33 +0300

    please ya rabe please

--- src/main/java/com/example/oopproject/controllers/DiagnosticsController.java
+++ src/main/java/com/example/oopproject/controllers/DiagnosticsController.java
@@ -1,4 +1,4 @@
-package com.example.oopproject.controller;
+package com.example.oopproject.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
